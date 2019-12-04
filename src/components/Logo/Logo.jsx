128f5057eef99a42909bd02dace58bda8e436960
Konstantin Bulba <ironypk@gmail.com>
2019-12-04 16:39:04 +1200

    change logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import "./Logo.css";
+import style from "./Logo.module.css";
 import {Link} from "react-router-dom";
 
 
 
 let Logo = () => {
     return (
-      <Link to='#' className="logo">
-          <div className="logo__one">Loft</div>
-          <div className="logo__two">Taxi</div>
+      <Link to='#' className={style.logo}>
+          <div className={`logo__one ${style.logo__one}`}>Loft</div>
+          <div className={`logo__two ${style.logo__two}`}>Taxi</div>
       </Link>
     );
   };
