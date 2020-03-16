942aa8f383f25f31beb08ad47f33d41686d5a2bf
Konstantin Bulba <ironypk@gmail.com>
2020-03-16 12:31:02 +1200

    add login data

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,12 +7,15 @@ import { Route, Switch } from "react-router-dom";
 
 let Welcome = ({ match }) => {
   return (
-    <div data-testid = 'welcome' className={s.welcome}>
+    <div data-testid="welcome" className={s.welcome}>
       <div className={s.welcome__logo}>
         <Logo />
+        <div style={{ backgroundColor: "#fff",color:'#000',padding:20 }}>
+          <div><div>Имя пользователя - a</div><div>Пароль - a</div></div>
+        </div>
       </div>
       <Switch>
-        <Route path={`${match.path}/`} component={LoginContainer} exact/>
+        <Route path={`${match.path}/`} component={LoginContainer} exact />
         <Route path={`${match.path}/regist`} component={RegistContainer} />
       </Switch>
     </div>
